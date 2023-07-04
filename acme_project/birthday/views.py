252e70da3104896ab5c0fefeec74a54173181cf1
babichdenis <55616713+babichdenis@users.noreply.github.com>
2023-07-04 09:08:16 +0200

    mixing

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -4,7 +4,13 @@ from .models import Birthday
 from .forms import BirthdayForm
 from .utils import calculate_birthday_countdown
 from django.core.paginator import Paginator
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
+
+
+
+'''
 def birthday(request, pk=None):
     if pk is not None:
         instance = get_object_or_404(Birthday, pk=pk)
@@ -29,19 +35,10 @@ def birthday(request, pk=None):
 
 
 def birthday_list(request):
-    # Получаем список всех объектов с сортировкой по id.
     birthdays = Birthday.objects.order_by('id')
-    # Создаём объект пагинатора с количеством 10 записей на страницу.
     paginator = Paginator(birthdays, 10)
-
-    # Получаем из запроса значение параметра page.
     page_number = request.GET.get('page')
-    # Получаем запрошенную страницу пагинатора. 
-    # Если параметра page нет в запросе или его значение не приводится к числу,
-    # вернётся первая страница.
     page_obj = paginator.get_page(page_number)
-    # Вместо полного списка объектов передаём в контекст 
-    # объект страницы пагинатора
     context = {'page_obj': page_obj}
     return render(request, 'birthday/birthday_list.html', context) 
 
@@ -61,3 +58,32 @@ def delete_birthday(request, pk):
         return redirect('birthday:list')
     # Если был получен GET-запрос — отображаем форму.
     return render(request, 'birthday/birthday.html', context)
+'''
+
+
+class BirthdayListView(ListView):
+    model = Birthday
+    ordering = 'id'
+    paginate_by = 10 
+    
+    
+class BirthdayMixin:
+    model = Birthday
+    success_url = reverse_lazy('birthday:list')
+
+
+class BirthdayFormMixin:
+    form_class = BirthdayForm
+    template_name = 'birthday/birthday.html'
+
+
+class BirthdayCreateView(BirthdayMixin, BirthdayFormMixin, CreateView):
+    pass
+
+
+class BirthdayUpdateView(BirthdayMixin, BirthdayFormMixin, UpdateView):
+    pass
+
+
+class BirthdayDeleteView(BirthdayMixin, DeleteView):
+    pass 
